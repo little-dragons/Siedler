881a1d4a70c5fcab3075df7c3a008e9f6c636160
IchiganCS <mail@ichigancs.com>
2023-11-07 12:08:59 +0100

    Removed matrix4x4 extensions

--- Source/Lini/Source/Graph/Components/BuiltIn/Camera.cs
+++ Source/Lini/Source/Graph/Components/BuiltIn/Camera.cs
@@ -6,7 +6,7 @@ namespace Lini.Graph.Components.BuiltIn;
 public struct Camera : IComponent
 {
     static int IComponent.TypeID { get; set; }
-    
+
     public Entity Entity;
 
     public float FieldOfView;
@@ -15,9 +15,15 @@ public struct Camera : IComponent
     public float AspectRatio;
 
 
-    public readonly Matrix4x4 ViewMatrix =>
-        Matrix4x4Extensions.CreateLookAt(Entity.AbsoluteTransform);
+    public readonly Matrix4x4 ViewMatrix
+    {
+        get
+        {
+            Matrix4x4.Invert(Entity.AbsoluteTransform, out var res);
+            return res;
+        }
+    }
 
-    public readonly Matrix4x4 ProjectionMatrix =>
-        Matrix4x4.CreatePerspectiveFieldOfView(FieldOfView, AspectRatio, NearPlane, FarPlane);
+    public readonly Matrix4x4 ProjectionMatrix
+        => Matrix4x4.CreatePerspectiveFieldOfView(FieldOfView, AspectRatio, NearPlane, FarPlane);
 }
